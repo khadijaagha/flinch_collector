49b813eba845812c708783939cfc333a2efb0142
khadijaagha <dija28@gmail.com>
2023-06-19 19:47:01 +0100

    final

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,25 @@
 from django.urls import path
 from . import views 
 
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('finch/', views.finches_index, name='index'),
+    path('finch/<int:finch_id>/', views.finches_detail, name='detail'),
+    path('finch/create/', views.FinchCreate.as_view(), name='finches_create'),
+    path('finch/<int:pk>/update/', views.FinchUpdate.as_view(), name='finches_update'),
+    path('finch/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
+    path('finch/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+    # associate a toy with a finch (M:M)
+    path('finch/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+    #unassociate a toy with a finch  
+    path('finch/<int:finch_id>/cancel_assoc_toy/<int:toy_id>/', views.cancel_assoc_toy, name='cancel_assoc_toy'),
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+
+    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,92 @@
-from django.shortcuts import render
-finchcollector = [
-  {'name': 'Lolo', 'breed': 'goldfinch', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'parrot crossbukk', 'description': 'talkative', 'age': 2},
-]
+import uuid
+import boto3
+import os
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
+from .forms import FeedingForm
+
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    id_list = finch.toys.all().values_list('id')
+    # instantiate feeding form to be rendered
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {
+        'finch': finch, 'feeding_form': feeding_form,
+        'toys': toys_finch_doesnt_have
+    })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+
+    #__all__ tells django to render all fields for the input i.e. name, breed, description, age - its like a shortcut 
+
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age'] #order matters
+
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finch'
+
+
+def add_feeding(request, finch_id):
+    #create a model form instance using the data that was submitted in the form
+    form = FeedingForm(request.POST)
+    #must validate the form
+    if form.is_valid():
+        #we want a model instance but cant save it to the db yet cos we havent assigned cat_id FK
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id) 
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys'
+
+
+  #associating a toy with a finch
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def cancel_assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+
